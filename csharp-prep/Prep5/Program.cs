320ef384d18024c9de4d0e0b956ac5c9c84aecdc
t-sibanda <tsibanda@usapglobal.org>
2023-05-24 23:49:08 -0400

    Completed Assignment prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void DisplayWelcome()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    public static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    public static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    public static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine("{0}, the square of your number is {1}", name, number);
+    }
+
+    public static void Main(string[] args)
+    {
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(name, squaredNumber);
     }
 }
