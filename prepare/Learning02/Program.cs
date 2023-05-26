585d28ea88b3338cfbc6ff15e4793682f26a7be2
t-sibanda <tsibanda@usapglobal.org>
2023-05-26 00:00:11 -0400

    Learning activity 2 for submission

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        // Create a new job.
+        Job job1 = new Job("Facilities Manager", "Dawn Properties", 2021, 2022);
+
+        // Create a second job.
+        Job job2 = new Job("Account Manager", "Vivo Energy", 2020, 2021);
+
+        // Create a new resume.
+        Resume resume = new Resume("Terrence Sibanda");
+
+        // Add the jobs to the resume.
+        resume.AddJob(job1);
+        resume.AddJob(job2);
+
+        // Display the resume.
+        resume.Display();
     }
 }
