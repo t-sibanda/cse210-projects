7cf0e80653cc38def81088132e1efadc477e233a
tisibanda <tsibanda@usapglobal.org>
2024-11-06 16:24:45 -0500

    commit- prep 5 functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,34 +1,28 @@
 using System;
-
 public class Program
 {
     public static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the Program!");
     }
-
     public static string PromptUserName()
     {
         Console.WriteLine("Please enter your name: ");
         return Console.ReadLine();
     }
-
     public static int PromptUserNumber()
     {
         Console.WriteLine("Please enter your favorite number: ");
         return int.Parse(Console.ReadLine());
     }
-
     public static int SquareNumber(int number)
     {
         return number * number;
     }
-
     public static void DisplayResult(string name, int number)
     {
         Console.WriteLine("{0}, the square of your number is {1}", name, number);
     }
-
     public static void Main(string[] args)
     {
         DisplayWelcome();
