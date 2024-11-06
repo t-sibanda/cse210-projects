4c87e2d0d232f658257a37fe459c79e1ab853976
tisibanda <tsibanda@usapglobal.org>
2024-11-06 16:20:03 -0500

    commit- prep 4 lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,10 +4,8 @@ class ListOfNumbers
 {
     static void Main(string[] args)
     {
-        // Create a list to store the numbers.
         List<int> numbers = new List<int>();
 
-        // Ask the user for a series of numbers, and append each one to the list. Stop when they enter 0.
         int number;
         do
         {
@@ -20,17 +18,14 @@ class ListOfNumbers
             }
         } while (number != 0);
 
-        // Compute the sum, or total, of the numbers in the list.
         int sum = 0;
         foreach (int num in numbers)
         {
             sum += num;
         }
 
-        // Compute the average of the numbers in the list.
         float average = (float)sum / numbers.Count;
 
-        // Find the maximum, or largest, number in the list.
         int max = numbers[0];
         foreach (int num in numbers)
         {
@@ -40,7 +35,6 @@ class ListOfNumbers
             }
         }
 
-        // Find the smallest positive number (the positive number that is closest to zero).
         int smallestPositive = numbers[0];
         foreach (int num in numbers)
         {
@@ -50,10 +44,8 @@ class ListOfNumbers
             }
         }
 
-        // Sort the numbers in the list and display the new, sorted list. Hint: There are C# libraries that can help you here, try searching the internet for them.
         List<int> sortedNumbers = numbers.OrderBy(x => x).ToList();
 
-        // Display the results.
         Console.WriteLine("The sum is: {0}", sum);
         Console.WriteLine("The average is: {0}", average);
         Console.WriteLine("The largest number is: {0}", max);
